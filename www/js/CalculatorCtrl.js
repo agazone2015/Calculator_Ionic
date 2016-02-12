19d2bb409d39ab2ffeeb136dc13bee7ca3d514d3
agazone2015 <agazone@hotmail.com>
2016-02-12 13:25:25 +1100

    pending calculation

--- www/js/CalculatorCtrl.js
+++ www/js/CalculatorCtrl.js
@@ -1,17 +1,70 @@
-angular.module('calculator', [])
-
-.controller('CalculatorCtrl', function($scope) {
-    $scope.result = '';
+angular.module("calculator", [])
     
-    $scope.btnClicked =  function(btn) {
-        if (btn == 'C') {
-            $scope.result = '';
-        }
-        else if (btn == '=') {
-            $scope.result = eval($scope.result);
-        }
-        else{
-            $scope.result += btn;
-        }
-    };
-});
+.controller("CalculatorCtrl", function CalculatorCtrl($scope) {
+		$scope.displayValue = '';
+		$scope.memory = null;
+		$scope.result = '';
+		$scope.operation = '';
+
+		$scope.saveInMemory = function(btn) {
+                       alert('button clicked');
+			if (btn == '') {
+				$scope.saveInMemory = $scope.memory;
+			}
+		};
+		$scope.btnClickedClear = function(btn) {
+			if (btn == 'C') {
+			     $scope.memory = null;
+			     $scope.displayValue = 0;
+			     $scope.result = 0;  
+			}
+			
+		};
+    
+		$scope.btnClickedSum = function(btn) {
+            alert('button clicked');
+            if ( btn == '+') {
+                $scope.saveInMemory();
+			    $scope.result = '';
+            }	
+		};
+    
+		$scope.btnClickedSubtract = function(btn) {
+            if (btn == '-') {
+                $scope.saveInMemory();
+			    $scope.result = '';
+            }
+		};
+    
+		$scope.btnClickedMultiply = function(btn) {
+            if (btn == '*') {
+               $scope.saveInMemory();
+			   $scope.result = ''; 
+            }
+		};
+    
+		$scope.btnClickedDivide = function(btn) {
+            if (btn == '/') {
+                $scope.saveInMemory();
+			    $scope.result = '';
+            }
+		};
+    
+		$scope.btnClickedCalculate = function(btn) {
+			if ($scope.operation == "+"){
+				$scope.result = parseFloat($scope.memory) + parseFloat($scope.displayValue);
+			}
+			else if ($scope.operation == "-"){
+				$scope.result = parseFloat($scope.memory) - parseFloat($scope.displayValue);
+			}
+			else if ($scope.operation == "*"){
+				$scope.result = parseFloat($scope.memory) * parseFloat($scope.displayValue);
+			}
+			else if ($scope.operation == "/"){
+				$scope.result = parseFloat($scope.memory) / parseFloat($scope.displayValue);
+			}
+
+			$scope.memory = $scope.result;
+		};
+	}
+);
